9abd50cb000dcb4a30ebaa22f15c051aadff7d69
Hamish Currie <hamish@currie.com.au>
2017-04-29 22:44:23 +1000

    initial

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { shallow } from 'enzyme';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+it('renders without crashing in Enzyme', () => {
+  shallow(<App />);
+});
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to React</h2>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
